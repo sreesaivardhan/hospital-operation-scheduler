36b7c0c0401c4190989edcf4b5918034a33a6393
sirasanagandla sree sai vardhan <163175549+sreesaivardhan@users.noreply.github.com>
2025-08-28 06:12:08 +0530

    Delete src/firebase-config.js

--- src/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js - Browser Compatible Version
-window.firebaseConfig = {
-    apiKey: "AIzaSyDbM9lHRMqKm4q9HGsMaLoSYheHyG778oY",
-    authDomain: "hospital-scheduler-2025.firebaseapp.com",
-    projectId: "hospital-scheduler-2025",
-    storageBucket: "hospital-scheduler-2025.appspot.com",
-    messagingSenderId: "707345795665",
-    appId: "1:707345795665:web:adbc753023afffb8ca0670"
-};
